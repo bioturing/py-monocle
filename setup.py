01bb70e8687ef7dd4ab2ec2fba4852f594aa0548
nhatnm52 <minhnhatnguyen5202@gmail.com>
2024-02-15 08:01:21 +0000

    Refactor: change base folder name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ BASE_IMPORT = "py_monocle"
 
 
 def _get_version():
-  version_path = os.path.join(__DIRNAME__, "py_monocle3", "_version.py")
+  version_path = os.path.join(__DIRNAME__, "py_monocle", "_version.py")
   variables = {}
   with open(version_path, encoding="utf-8") as f:
     # pylint: disable=exec-used
@@ -32,7 +32,7 @@ setup(
     url="https://alpha.bioturing.com",
     description="BioTuring Py-Monocle3",
     long_description="",
-    package_dir={BASE_IMPORT: "py_monocle3"},
+    package_dir={BASE_IMPORT: "py_monocle"},
     packages=[BASE_IMPORT],
     zip_safe=False,
     python_requires=">=3.8, <3.12",
