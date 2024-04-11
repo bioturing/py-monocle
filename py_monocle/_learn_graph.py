23f22293fa3825668e93d1ebebfc7291b60e7c68
nhatnm52 <minhnhatnguyen5202@gmail.com>
2024-04-11 09:12:37 +0000

    Set MINIMUM_CENTROIDS for principal graph

--- py_monocle/_learn_graph.py
+++ py_monocle/_learn_graph.py
@@ -7,11 +7,14 @@ from sklearn.cluster import KMeans
 import networkx as nx
 
 from ._utils import (
-  minimum_spanning_tree,
-  find_nearest_principal_edges,
-  pairwise_distance,
-  compute_cluster_connectivities,
+    minimum_spanning_tree,
+    find_nearest_principal_edges,
+    pairwise_distance,
+    compute_cluster_connectivities,
 )
+
+
+MINIMUN_CENTROIDS = 256
 
 
 def prune_tree(
@@ -62,13 +65,13 @@ def prune_tree(
 
 
 def connect_tip_cells(
-  matrix: np.ndarray,
-  mst: sparse.csr_matrix,
-  centroids: np.ndarray,
-  clusters: np.ndarray,
-  qval_thresh: float = 0.05,
-  euclidean_distance_ratio: float = 1,
-  geodesic_distance_ratio: float = 1/3,
+    matrix: np.ndarray,
+    mst: sparse.csr_matrix,
+    centroids: np.ndarray,
+    clusters: np.ndarray,
+    qval_thresh: float = 0.05,
+    euclidean_distance_ratio: float = 1,
+    geodesic_distance_ratio: float = 1/3,
 ):
   """Find all tip cells and check if they fit criteria to add connections.
 
@@ -107,9 +110,11 @@ def connect_tip_cells(
   degrees = np.diff(mst.indptr)
   leaves = np.where(degrees == 1)[0]
 
-  cluster_mat = compute_cluster_connectivities(matrix=matrix, clusters=clusters)
+  cluster_mat = compute_cluster_connectivities(
+      matrix=matrix, clusters=clusters)
   cluster_mat_leaf = cluster_mat[leaves][:, leaves]
-  np.fill_diagonal(cluster_mat_leaf, qval_thresh + 1) # ignore connections of leaves to itself
+  # ignore connections of leaves to itself
+  np.fill_diagonal(cluster_mat_leaf, qval_thresh + 1)
   valid_connection = np.where(cluster_mat_leaf < qval_thresh)
   joined_indices = []
 
@@ -119,7 +124,7 @@ def connect_tip_cells(
     leaf_1 = leaves[i]
     leaf_2 = leaves[j]
     if euclidean_distance_ratio * longest_edge > centroids[leaf_1] @ centroids[leaf_2].T and \
-      geodesic_distance_ratio * diameter <= distance[leaf_1][leaf_2]:
+            geodesic_distance_ratio * diameter <= distance[leaf_1][leaf_2]:
       joined_indices.append([leaf_1, leaf_2])
   joined_indices = np.array(joined_indices, dtype=int)
   if len(joined_indices) > 0:
@@ -129,9 +134,9 @@ def connect_tip_cells(
 
 
 def calculate_projection(
-  matrix: np.ndarray,
-  roots: np.ndarray,
-  leaves: np.ndarray,
+    matrix: np.ndarray,
+    roots: np.ndarray,
+    leaves: np.ndarray,
 ) -> np.ndarray:
   """
   Calculate the projection of a cells onto the nearest edges of principal graph.
@@ -150,7 +155,8 @@ def calculate_projection(
     Coordinate of the projection of the cells onto the nearest edges.
   """
   edge_length = np.linalg.norm(leaves - roots, axis=1) ** 2
-  proj_factor = np.sum((matrix - roots) * (leaves - roots), axis=1) / edge_length
+  proj_factor = np.sum(
+      (matrix - roots) * (leaves - roots), axis=1) / edge_length
   matrix = roots + proj_factor.reshape((-1, 1)) * (leaves - roots)
   matrix[proj_factor < 0] = roots[proj_factor < 0]
   matrix[proj_factor > 1] = leaves[proj_factor > 1]
@@ -179,10 +185,10 @@ def project_cells(
     Coordinates of projected cells onto principal graph.
   """
   nearest_nodes, second_vertices = find_nearest_principal_edges(
-    matrix, centroids, mst
+      matrix, centroids, mst
   )
   return calculate_projection(
-    matrix, centroids[nearest_nodes], centroids[second_vertices])
+      matrix, centroids[nearest_nodes], centroids[second_vertices])
 
 
 def __calc_objective_centroids(
@@ -222,54 +228,61 @@ def __learn_graph(
 ):
   if use_clusters_as_kmeans:
     assert clusters is not None, \
-      "Request clusters for option ``use_clusters_as_kmeans``"
+        "Request clusters for option ``use_clusters_as_kmeans``"
     components, labels = np.unique(clusters, return_inverse=True)
     n_centroids = len(components)
   else:
     if n_centroids is None:
       n_cluster = len(np.unique(clusters)) if clusters is not None else 1
-      n_centroids = int(n_cluster * centroids_per_log * np.log10(matrix.shape[0]))
+      n_centroids = int(n_cluster * centroids_per_log *
+                        np.log10(matrix.shape[0]))
+    n_centroids = max(n_centroids, MINIMUN_CENTROIDS)
     n_centroids = min(n_centroids, matrix.shape[0])
 
-    # Init centroidsnit
-    np.random.seed(kmeans_seed)
-    init = matrix[np.linspace(0, matrix.shape[0] - 1, num=n_centroids, dtype=int)]
-    init += 1e-10 * np.random.standard_normal(size=(n_centroids, 1))
-    solver = KMeans(
-      n_clusters=n_centroids,
-      init=init, max_iter=100,
-      random_state=kmeans_seed,
-      n_init=1,
-    )
-    labels = solver.fit_predict(matrix)
+    if n_centroids < matrix.shape[0]:
+      # Init centroids
+      np.random.seed(kmeans_seed)
+      init = matrix[np.linspace(0, matrix.shape[0] - 1,
+                                num=n_centroids, dtype=int)]
+      init += 1e-10 * np.random.standard_normal(size=(n_centroids, 1))
+      solver = KMeans(
+          n_clusters=n_centroids,
+          init=init, max_iter=100,
+          random_state=kmeans_seed,
+          n_init=1,
+      )
+      labels = solver.fit_predict(matrix)
 
-  kdtree = KDTree(matrix)
-  knn_dists, _ = kdtree.query(matrix, k_nn)
-  rho = np.exp(- np.mean(knn_dists, axis=1)) + 1e-6
-  indices = sparse.csr_matrix(
-    (rho, labels, np.arange(len(rho)))
-  ).argmax(axis=0)
-  centroids = matrix[np.array(indices).flatten()]
+  if n_centroids == matrix.shape[0]:
+    centroids = matrix
+  else:
+    kdtree = KDTree(matrix)
+    knn_dists, _ = kdtree.query(matrix, k_nn)
+    rho = np.exp(- np.mean(knn_dists, axis=1)) + 1e-6
+    indices = sparse.csr_matrix(
+        (rho, labels, np.arange(len(rho)))
+    ).argmax(axis=0)
+    centroids = matrix[np.array(indices).flatten()]
 
-  # Optimize centroids of principal graph
-  objective_score = 0.001
-  for _ in range(max_iter):
-    mst = minimum_spanning_tree(centroids)
-    sym_mst = mst.toarray()
-    weight = np.sum(sym_mst)
-    connected = sym_mst != 0
+    # Optimize centroids of principal graph
+    objective_score = 0.001
+    for _ in range(max_iter):
+      mst = minimum_spanning_tree(centroids)
+      sym_mst = mst.toarray()
+      weight = np.sum(sym_mst)
+      connected = sym_mst != 0
 
-    probs, obj_centroid = __calc_objective_centroids(
-      matrix, centroids, sigma=sigma)
-    objective = weight + gamma * obj_centroid
-    if abs(objective - objective_score) / objective_score < eps:
-      break
-    objective_score = objective
+      probs, obj_centroid = __calc_objective_centroids(
+          matrix, centroids, sigma=sigma)
+      objective = weight + gamma * obj_centroid
+      if abs(objective - objective_score) / objective_score < eps:
+        break
+      objective_score = objective
 
-    Q = 2 * (np.diag(np.sum(connected, axis=0)) - connected) + \
-      gamma * np.diag(np.sum(probs, axis=0))
-    B = gamma * matrix.T @ probs
-    centroids = (B @ np.linalg.inv(Q)).T
+      Q = 2 * (np.diag(np.sum(connected, axis=0)) - connected) + \
+          gamma * np.diag(np.sum(probs, axis=0))
+      B = gamma * matrix.T @ probs
+      centroids = (B @ np.linalg.inv(Q)).T
 
   # Create principal graph
   mst = minimum_spanning_tree(centroids)
@@ -356,9 +369,32 @@ def learn_graph(
     for partition in np.unique(partitions):
       indices = [partitions == partition]
       sub_mst, sub_centroids = __learn_graph(
-        matrix=matrix[indices],
+          matrix=matrix[indices],
+          n_centroids=n_centroids,
+          clusters=clusters[indices] if clusters is not None else None,
+          centroids_per_log=centroids_per_log,
+          prune=prune,
+          p_threshold=p_threshold,
+          form_circle=form_circle,
+          kmeans_seed=kmeans_seed,
+          max_iter=max_iter,
+          k_nn=k_nn,
+          sigma=sigma,
+          gamma=gamma,
+          eps=eps,
+          use_clusters_as_kmeans=use_clusters_as_kmeans,
+      )
+      if partition == 0:
+        mst = sub_mst
+      else:
+        mst = sparse.bmat([[mst, None], [None, sub_mst]])
+      centroids.append(sub_centroids)
+    centroids = np.concatenate(centroids, axis=0)
+  else:
+    mst, centroids = __learn_graph(
+        matrix=matrix,
         n_centroids=n_centroids,
-        clusters=clusters[indices] if clusters is not None else None,
+        clusters=clusters,
         centroids_per_log=centroids_per_log,
         prune=prune,
         p_threshold=p_threshold,
@@ -370,29 +406,6 @@ def learn_graph(
         gamma=gamma,
         eps=eps,
         use_clusters_as_kmeans=use_clusters_as_kmeans,
-      )
-      if partition == 0:
-        mst = sub_mst
-      else:
-        mst = sparse.bmat([[mst, None], [None, sub_mst]])
-      centroids.append(sub_centroids)
-    centroids = np.concatenate(centroids, axis=0)
-  else:
-    mst, centroids = __learn_graph(
-      matrix=matrix,
-      n_centroids=n_centroids,
-      clusters=clusters,
-      centroids_per_log=centroids_per_log,
-      prune=prune,
-      p_threshold=p_threshold,
-      form_circle=form_circle,
-      kmeans_seed=kmeans_seed,
-      max_iter=max_iter,
-      k_nn=k_nn,
-      sigma=sigma,
-      gamma=gamma,
-      eps=eps,
-      use_clusters_as_kmeans=use_clusters_as_kmeans,
     )
   projected_points = project_cells(matrix, centroids, mst)
   return projected_points, mst, centroids
